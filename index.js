a04a473be40e114fdef4494e1f9c61401be73321
mbron770 <mbronfin.tsd@gmail.com>
2023-05-29 00:17:05 -0400

    add obj functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee = {name: "x", streetAddress: '123' }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, 
+        [key]: value, 
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const tempVar = {...employee}
+    delete tempVar[key]
+    return tempVar
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
+
